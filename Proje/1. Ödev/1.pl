dd3459658a792fc9b90c6c222c1933b6de8c1c27
DENİZ ÖZCAN <94781420+deniz-ozcan@users.noreply.github.com>
2023-04-10 23:35:26 +0300

    Semptomlar girildi

--- Proje/1. Ödev/1.pl
+++ Proje/1. Ödev/1.pl
@@ -1,1 +1,32 @@
+% Define the symptoms for each disease
+symptom(korona, fever).
+symptom(korona, cough).
+symptom(korona, shortness_of_breath).
+symptom(korona, fatigue).
+symptom(korona, headache).
 
+symptom(domuz_gribi, fever).
+symptom(domuz_gribi, cough).
+symptom(domuz_gribi, sore_throat).
+symptom(domuz_gribi, body_aches).
+symptom(domuz_gribi, headache).
+
+symptom(sinuzit, facial_pain_or_pressure).
+symptom(sinuzit, stuffy_or_runny_nose).
+symptom(sinuzit, headache).
+symptom(sinuzit, cough).
+
+symptom(soguk_alginligi, runny_nose).
+symptom(soguk_alginligi, congestion).
+symptom(soguk_alginligi, sore_throat).
+symptom(soguk_alginligi, cough).
+symptom(soguk_alginligi, sneezing).
+symptom(soguk_alginligi, headache).
+
+symptom(grip, fever).
+symptom(grip, cough).
+symptom(grip, sore_throat).
+symptom(grip, runny_nose).
+symptom(grip, body_aches).
+symptom(grip, headache).
+symptom(grip, fatigue).
