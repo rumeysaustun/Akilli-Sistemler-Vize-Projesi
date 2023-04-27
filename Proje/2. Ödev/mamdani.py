636d7422f410c39749f1254b429c9813e55f078e
Sule <95585677+Sulecck@users.noreply.github.com>
2023-04-27 15:52:33 +0300

    Update mamdani.py

--- Proje/2. Ödev/mamdani.py
+++ Proje/2. Ödev/mamdani.py
@@ -19,23 +19,22 @@ nem['kuru'] = fuzz.trimf(nem.universe, [0, 0, 50])
 nem['normal'] = fuzz.trimf(nem.universe, [0, 50, 100])
 nem['nemli'] = fuzz.trimf(nem.universe, [50, 100, 100])
 
-pencere_acikligi['kapali'] = fuzz.trimf(pencere_acikligi.universe, [0, 0, 25])
-pencere_acikligi['yarim_acik'] = fuzz.trimf(pencere_acikligi.universe, [0, 25, 50])
-pencere_acikligi['orta_acik'] = fuzz.trimf(pencere_acikligi.universe, [25, 50, 75])
-pencere_acikligi['tam_acik'] = fuzz.trimf(pencere_acikligi.universe, [50, 100, 100])
+pencere_acikligi['kapali'] = fuzz.trimf(pencere_acikligi.universe, [0, 0, 50])
+pencere_acikligi['yarim_acik'] = fuzz.trimf(pencere_acikligi.universe, [0, 50, 100])
+pencere_acikligi['acik'] = fuzz.trimf(pencere_acikligi.universe, [50, 100, 100])
 
 # Kuralların tanımlanması
 kural1 = ctrl.Rule(sicaklik['soğuk'] & nem['kuru'], pencere_acikligi['kapali'])
 kural2 = ctrl.Rule(sicaklik['soğuk'] & nem['normal'], pencere_acikligi['yarim_acik'])
-kural3 = ctrl.Rule(sicaklik['soğuk'] & nem['nemli'], pencere_acikligi['tam_acik'])
+kural3 = ctrl.Rule(sicaklik['soğuk'] & nem['nemli'], pencere_acikligi['acik'])
 
-kural4 = ctrl.Rule(sicaklik['ilik'] & nem['kuru'], pencere_acikligi['orta_acik'])
-kural5 = ctrl.Rule(sicaklik['ilik'] & nem['normal'], pencere_acikligi['orta_acik'])
-kural6 = ctrl.Rule(sicaklik['ilik'] & nem['nemli'], pencere_acikligi['tam_acik'])
+kural4 = ctrl.Rule(sicaklik['ilik'] & nem['kuru'], pencere_acikligi['yarim_acik'])
+kural5 = ctrl.Rule(sicaklik['ilik'] & nem['normal'], pencere_acikligi['yarim_acik'])
+kural6 = ctrl.Rule(sicaklik['ilik'] & nem['nemli'], pencere_acikligi['acik'])
 
 kural7 = ctrl.Rule(sicaklik['sicak'] & nem['kuru'], pencere_acikligi['kapali'])
 kural8 = ctrl.Rule(sicaklik['sicak'] & nem['normal'], pencere_acikligi['kapali'])
-kural9 = ctrl.Rule(sicaklik['sicak'] & nem['nemli'], pencere_acikligi['orta_acik'])
+kural9 = ctrl.Rule(sicaklik['sicak'] & nem['nemli'], pencere_acikligi['yarim_acik'])
 
 # Kontrol Sistem kurallarının bir araya getirilmesi
 pencere_acikligi_kontrol = ctrl.ControlSystem([kural1, kural2, kural3, kural4, kural5, kural6, kural7, kural8, kural9])
